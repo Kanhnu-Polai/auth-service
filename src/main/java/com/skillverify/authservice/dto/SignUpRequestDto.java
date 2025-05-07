8c4295ebd14e72dd2d380824644dfc67d21a5888
Kanhu Polai <pkanhu79@gmail.com>
2025-05-08 00:14:45 +0530

    Update sinup dto

--- src/main/java/com/skillverify/authservice/dto/SignUpRequestDto.java
+++ src/main/java/com/skillverify/authservice/dto/SignUpRequestDto.java
@@ -1,5 +1,15 @@
 package com.skillverify.authservice.dto;
 
+import com.skillverify.authservice.utils.Role;
+
+import lombok.Data;
+
+@Data
 public class SignUpRequestDto {
+	
+	private String name;
+	private String email;
+	private String password;
+	private Role role;
 
 }
