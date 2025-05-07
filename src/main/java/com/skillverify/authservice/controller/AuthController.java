9cfc55ee3f8dae9e68e482acc75665763f00cb26
Kanhu Polai <pkanhu79@gmail.com>
2025-05-07 17:47:37 +0530

    Basic set up done

---
+++ src/main/java/com/skillverify/authservice/controller/AuthController.java
@@ -0,0 +1,17 @@
+package com.skillverify.authservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+	
+	@GetMapping("/sinup")
+	public String signUp() {
+		return "Sign Up";
+	}
+
+}
